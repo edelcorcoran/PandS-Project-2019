21d5562f044a13d8d234ebb1a5787d2be1554a73
edelcorcoran <G00010824@gmit.ie>
2019-04-27 16:32:30 +0100

    linear_regression.py

---
+++ linear_regression.py
@@ -0,0 +1,14 @@
+
+#Linear Regression in Iris Dataset
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+iris = pd.read_csv('iris.csv')
+
+#seaborn lmplot to illustrate linear regression
+sns.lmplot(x = "SepalLengthCm", y = "SepalWidthCm", hue='Species', data=iris)
+#Show plot
+plt.show()
+
